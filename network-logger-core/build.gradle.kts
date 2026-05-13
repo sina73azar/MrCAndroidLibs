ba9005b36c912305222b33500445d93bfd7a1045
“Mr.C” <s.azar.engr@gmail.com>
2026-05-13 09:44:55 +0330

    Fix build

--- network-logger-core/build.gradle.kts
+++ network-logger-core/build.gradle.kts
@@ -27,8 +27,13 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
+   /* kotlinOptions {
         jvmTarget = "11"
+    }*/
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
     }
 }
 
